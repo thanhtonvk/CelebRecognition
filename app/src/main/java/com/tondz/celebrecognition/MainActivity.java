5d3e41ee1ecd0956088b832b1506262ffcd9c3f9
tondt <tondt@dyno.vn>
2024-08-25 21:57:59 +0700

    add

--- app/src/main/java/com/tondz/celebrecognition/MainActivity.java
+++ app/src/main/java/com/tondz/celebrecognition/MainActivity.java
@@ -2,16 +2,24 @@ package com.tondz.celebrecognition;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tondz.celebrecognition.views.NhanDienNguoiThanActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        startActivity(new Intent(getApplicationContext(), NhanDienNguoiThanActivity.class));
+        String PASSWORD = "12345";
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference();
+
+        reference.child("pass").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.getValue().toString().equals(PASSWORD)) {
+                    startActivity(new Intent(getApplicationContext(), NhanDienNguoiThanActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Đã hết thời gian dùng thử", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                finish();
+            }
+        });
+
+
     }
 }
